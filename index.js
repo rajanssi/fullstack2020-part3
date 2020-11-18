79be2b79b97024e8bcb87cd5b0bbd8458ae33f0b
Anssi Rajala <anssi.rajala@helsinki.fi>
2020-11-18 17:38:37 +0200

    Osa 3. Tehtävät 3.13-3.14

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const app = express()
 const cors = require('cors')
+const app = express()
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.static('build'))
@@ -59,8 +61,10 @@ app.get('/info', (req, res) => {
     res.send(`Phonebook has info for ${persons.length} people.<p>${new Date()}`)
 })
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+app.get('/api/persons', (request, response) => {
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -81,27 +85,28 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 app.post('/api/persons', (request, response) => {
-    const person = request.body
-    morgan.token('person', JSON.stringify(person))
+    const body = request.body
+    morgan.token('person', JSON.stringify(body))
 
-    if (!person.name || !person.number) {
+    if (!body.name || !body.number) {
         return response.status(400).json({
             error: 'name or number missing'
         })
     }
 
-    if (persons.find(p => p.name === person.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+    })
 
-    person.id = getRandomInt(10000)
+    //person.id = getRandomInt(10000)
     persons = persons.concat(person)
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
